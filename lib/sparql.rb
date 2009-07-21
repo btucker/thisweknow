0053eb2b01171c571c18366148723c6b87183acf
Ben Tucker <ben@btucker.net>
2009-07-21 16:07:38 -0400

    add logging to sparql class

--- lib/sparql.rb
+++ lib/sparql.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 
 class Sparql 
   def self.execute(query)
+    Rails.logger.info "[SPARQL] #{query}"
     Nokogiri::XML(open(URI.encode("http://206.192.70.249/data.gov/sparql.aspx?query=#{query}")))
   end
 end
