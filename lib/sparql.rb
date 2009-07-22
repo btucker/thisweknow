b66643fcef5f68801b5abfbf69bfc8c673c0351a
Ben Tucker <ben@btucker.net>
2009-07-22 14:48:09 -0400

    adding prefixes

--- lib/sparql.rb
+++ lib/sparql.rb
@@ -3,6 +3,17 @@ require 'open-uri'
 class Sparql 
   def self.execute(query)
     Rails.logger.info "[SPARQL] #{query}"
-    Nokogiri::XML(open(URI.encode("http://206.192.70.249/data.gov/sparql.aspx?query=#{query}")))
+    Nokogiri::XML(open(URI.encode("http://206.192.70.249/data.gov/sparql.aspx?query=#{prefixes + query}")))
+  end
+
+  private
+
+  def prefixes
+    %Q{
+      PREFIX o: <http://www.data.gov/ontology#>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    } 
   end
 end
