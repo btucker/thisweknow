d55e2bd62d8aac7bb169e83bc72ebeede9a2d918
Ben Tucker <ben@btucker.net>
2009-07-30 18:02:01 -0400

    integrating acts_as_geocodable

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,25 @@
 class City < ActiveRecord::Base
+  acts_as_geocodable :address => {:locality => :name, :region => :admin1_code, :country => :country_code}
+
+  def geocode
+    if geocoding
+      geocoding.geocode
+    else
+      create_geocoding(:geocode => Geocode.create(
+        :latitude => latitude,
+        :longitude => longitude,
+        :locality => name,
+        :region => admin1_code,
+        :country => 'US',
+        :precision => 'city'))
+    end
+  end
+
+  def to_location
+    returning Graticule::Location.new do |location|
+      [:locality, :region, :country].each do |attr|
+        location.send "#{attr}=", geo_attribute(attr)
+      end
+    end
+  end
 end
