22ba389edc92e4ef466bcba48bc3fc8f2a5fc5fb
Ben Tucker <ben@btucker.net>
2009-07-30 20:02:48 -0400

    everything all hooked up

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,7 +8,29 @@ class LocationsController < ApplicationController
   end
 
   def factoid
-
+    @factoid = Factoid.find(params[:id])
+    @result = @factoid.execute(@location)
+    
+    variables = []
+    @result.search("head variable").map{|var| var["name"]}.each_slice(2) do |slice|
+      variables << slice
+    end
+    @headings = variables.map{|v|v[0]}
+    
+    
+    @data = [] #The list of data as received in the query, not organized by company name
+    #render :xml => @result
+    #return    
+    @result.search("result").each do |r|
+      row = {}
+      	variables.each do |lit_var, uri_var|
+	        row[lit_var.to_sym] = [
+	        						r.search("binding[name=#{lit_var}] literal").first.content, 
+	        						r.search("binding[name=#{uri_var}] uri").first.content
+        						]
+	    end
+	    @data << row
+    end
   end
 
   def search
--- app/models/factoid.rb
+++ app/models/factoid.rb
@@ -1,6 +1,9 @@
 class Factoid < ActiveRecord::Base
 	
-  def execute(location, radius)
+  def execute(location, radius=250)
+    if factoid_type == 'Point'
+      radius = 0
+    end
     doc = Sparql.execute(query % [location.lat_min(radius).to_f, location.lat_max(radius).to_f, location.lon_min(radius).to_f, location.lon_max(radius).to_f])
     return doc
   end
