f1203afa5ab5b1f03e5c57688a78ff9f63c086c6
Ben Tucker <ben@btucker.net>
2009-08-07 03:13:50 -0400

    annotation browser/editor

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -4,14 +4,25 @@
 
 class AnnotationsController < ApplicationController
   def index
-    res = Sparql.execute("SELECT ?o ?label WHERE { ?s rdf:type ?o }")
-    @classes = res.search("result binding uri").map(&:content)
+    res = Sparql.execute("SELECT ?o WHERE { ?s rdf:type ?o }", :ruby)
+    @classes = res.map { |r| r[:o] }.compact
   end
 
   def show
     @example = Sparql.execute("SELECT ?s ?p ?o WHERE { ?s rdf:type <#{params[:uri]}> . ?s ?p ?o } LIMIT 50", :ruby)
     @attributes = Sparql.execute("SELECT ?p WHERE { <#{params[:uri]}> ui:attribute ?p }", :ruby).map{|a| a[:p]}
     @belongs_to = Sparql.execute("SELECT ?p WHERE { <#{params[:uri]}> ui:belongsTo ?p }", :ruby).map{|a| a[:p]}
-    @has_many = Sparql.execute("SELECT ?c ?p WHERE { ?c ui:belongsTo ?p . ?obj rdf:type ?c . ?obj ?p ?obj2 . ?obj2 rdf:type <#{params[:uri]}> }", :ruby)
+    @has_many = []
+    #@has_many = Sparql.execute("SELECT ?c ?p WHERE { ?c ui:belongsTo ?p . ?obj rdf:type ?c . ?obj ?p ?obj2 . ?obj2 rdf:type <#{params[:uri]}> }", :ruby)
+  end
+
+  def add_annotation
+    Sparql.execute("INSERT INTO <ui> { <#{params[:uri]}> ui:#{params[:t]} <#{params[:p]}> }")
+    redirect_to :action => 'show', :uri => params[:uri]
+  end
+
+  def remove_annotation
+    Sparql.execute("DELETE FROM <ui> { <#{params[:uri]}> ui:#{params[:t]} <#{params[:p]}> }")
+    redirect_to :action => 'show', :uri => params[:uri]
   end
 end
