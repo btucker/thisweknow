e0ac09ee74159d3f7cd46b4916316062cb4d6cf5
Ben Tucker <ben@btucker.net>
2009-08-08 00:16:40 -0400

    hoepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,24 @@ class PagesController < ApplicationController
     @body_classes = %w(homepg)
     @homepage = true
     @examples = ['New York, NY', '90210', 'Miami, FL', 'Brattleboro, VT']
+
+    @toxins_factoid = Factoid.find(2)
+    @most_toxins = @toxins_factoid.factoid_results.find(:all,
+                                                        :order => 'count1 DESC',
+                                                        :limit => 5)
+    @least_toxins = @toxins_factoid.factoid_results.find(:all,
+                                                         :order => 'count1 ASC',
+                                                         :limit => 5)
+
+    @crime_factoid = Factoid.find(7)
+    @most_crime = @toxins_factoid.factoid_results.find(:all,
+                                                       :order => 'count1 DESC',
+                                                       :limit => 5)
+    @least_crime = @toxins_factoid.factoid_results.find(:all,
+                                                        :conditions => 'count1 > 0',
+                                                        :order => 'count1 ASC',
+                                                        :limit => 5)
+
+
   end
 end
--- app/models/factoid_result.rb
+++ app/models/factoid_result.rb
@@ -1,2 +1,4 @@
 class FactoidResult < ActiveRecord::Base
+  belongs_to :city
+  belongs_to :factoid
 end
