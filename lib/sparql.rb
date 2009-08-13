c42625e2eeea2b13d47ea9f9b65904148eacaa8f
Ben Tucker <ben@btucker.net>
2009-08-13 11:21:42 -0400

    capitalizing all PREFIXs

--- lib/sparql.rb
+++ lib/sparql.rb
@@ -44,8 +44,8 @@ class Sparql
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-    prefix samp: <http://www.rdfabout.com/rdf/schema/uscensus/details/samp/>
+    PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+    PREFIX samp: <http://www.rdfabout.com/rdf/schema/uscensus/details/samp/>
     } 
   end
 
